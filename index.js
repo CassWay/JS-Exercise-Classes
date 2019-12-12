5d705fe860fff20c3714fe4c76b8cdc55b22bcd0
Wayne Cassity <webdev.cassity@gmail.com>
2019-12-11 20:36:40 -0800

    Car constructor built; -- previous someFood and toString working, had to restart;

--- index.js
+++ index.js
@@ -76,6 +76,13 @@ class Person {
 */
 
 class Car {
+  constructor(model, milesPerGallon){
+    this.model = model ;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0 ;
+    this.odometer = 0 ;
+  }
+
 
 }
 
